31235b7ad6b30aee8172b034fbffe97fdb01ad6e
GerardGhub <singian28@gmail.com>
2023-03-23 14:54:15 +0800

    Commit muna

---
+++ database/migrations/2023_03_23_063645_create_site_infos_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('site_infos', function (Blueprint $table) {
+            $table->id();
+            $table->text('about', 5000);
+            $table->text('refund', 5000);
+            $table->text('purchase_guide', 5000);
+            $table->text('privacy',5000);
+            $table->text('privacy');
+            $table->string('android_app_link');
+            $table->string('ios_app_link');
+            $table->string('facebook_link');
+            $table->string('twitter_link');
+            $table->string('instagram_link');
+            $table->string('copyright_text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('site_infos');
+    }
+};
